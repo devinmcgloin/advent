b76b4ed7377398580c9fbbfd38c6300c1fe18067
Devin McGloin <devin@devinmcgloin.com>
2016-05-19 15:59:54 -0400

    added more data logging

--- server.py
+++ server.py
@@ -13,6 +13,7 @@ def process_mesage():
     """Listens at /hooks for posts to that url."""
     data = json.loads(request.data)
     print("INCOMING MESSAGE")
+    print(data)
     user_response = data["messages"]["text"]
     user_id = data["appUser"]["_id"]
     name = data["appUser"]["givenName"]
