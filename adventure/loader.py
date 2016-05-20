b9c82f315cc09f39f769affed91e30d18f5b06ad
Devin McGloin <devin@devinmcgloin.com>
2016-05-20 12:14:14 -0400

    more friendly prompt and simple messages

--- adventure/loader.py
+++ adventure/loader.py
@@ -12,6 +12,7 @@ import os
 import re
 from .data import parse
 import redis
+import logging
 
 r = redis.from_url(os.environ.get("REDIS_URL"))
 #r = redis.Redis(host='localhost', port=6379)
@@ -21,26 +22,25 @@ def user_exists(user_id):
 
 def new_game(user_id, seed=None):
     """Create new game"""
-    print("CREATING NEW GAME")
+    logging.debug("creating new came for {}".format(user_id))
     game = Game(seed)
     load_advent_dat(game)
     game.start()
-    response = game.output
-    print(response)
+    response = format_response(game.output)
     r.set(user_id, game.t_suspend())
-    print("RESPONDING")
     return response
 
 def respond(user_id, user_response):
     """Gets the game response for a specific user_id and user_response"""
     game = Game.resume(r.get(user_id))
-    user_tupl_resp = tuple(user_response.split(" "))
-    response = game.do_command(user_tupl_resp)
+    user_tupl_resp = tuple(user_response.lower().split(" "))
+    response = format_response(game.do_command(user_tupl_resp))
     r.set(user_id, game.t_suspend())
     return response
 
 def reset_game(user_id, seed=None):
-    """Clears the game for a specific user_id, need to wipe memory and file game"""
+    """Clears the game for a specific user_id, need to wipe Memory and file game"""
+    logging.debug("deleting save for {}".format(user_id))
     r.delete(user_id)
 
 def load_advent_dat(data):
@@ -48,3 +48,7 @@ def load_advent_dat(data):
     datapath = os.path.join(os.path.dirname(__file__), 'advent.dat')
     with open(datapath, 'r') as datafile:
         parse(data, datafile)
+
+def format_response(response):
+    clean_response = response.replace("\n"," ").lower().strip().capitalize().split(".")
+    return ".\n".join(clean_response)
