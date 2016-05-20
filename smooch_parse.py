cca280dc0eb3a4d71217f691fd747d06cd443988
Devin McGloin <devin@devinmcgloin.com>
2016-05-20 11:17:55 -0400

    typos

--- smooch_parse.py
+++ smooch_parse.py
@@ -1,6 +1,6 @@
 def get_user_id(conversation):
     return conversation["appUser"]["_id"]
 
-def most_recent_msg(converstation, msg_from="appUser"):
-    msessages = [ x for x in conversation["messages"] if x["role"] == "appUser"]
+def most_recent_msg(conversation, msg_from="appUser"):
+    messages = [ x for x in conversation["messages"] if x["role"] == "appUser"]
     return messages[0]
