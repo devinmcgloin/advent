bbe80c35def55ab3e6d7ecb04b6e5be8bd0f8b5d
Devin McGloin <devin@devinmcgloin.com>
2016-06-03 17:03:25 -0400

    spacing messages less agressively

--- conv_mechanics/scheduler.py
+++ conv_mechanics/scheduler.py
@@ -1,7 +1,16 @@
+import random
 import smooch
 
 
 def respond(user_id, response):
-    smooch.send_message(user_id, response)
-    # for msg in response.split("\n"):
-    #     smooch.send_message(user_id, msg)
+
+    messages = response.split("\n")
+
+    spacing = random.randint(2, 4)
+    start = 0
+    stop = spacing
+
+    while messages[start:stop]:
+        smooch.send_message(user_id, "\n".join(messages[start:stop]))
+        start = stop
+        stop += spacing
