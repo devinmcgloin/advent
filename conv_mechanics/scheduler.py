25892dd74551b2f7e2ae8f03beb41a825f62fd94
Devin McGloin <devin@devinmcgloin.com>
2016-06-05 00:27:22 -0400

    no timing for sending responses

--- conv_mechanics/scheduler.py
+++ conv_mechanics/scheduler.py
@@ -7,17 +7,4 @@ def respond(user_id, response):
     if response == "" or user_id == "":
         return
 
-    messages = response.split("\n")
-
-    stop = random.randint(2, 4)
-    start = 0
-
-    while messages[start:stop]:
-        msg = "\n".join(messages[start:stop])
-
-        if msg == "":
-            continue
-
-        smooch.send_message(user_id, msg)
-        start = stop
-        stop += random.randint(2, 4)
+    smooch.send_message(user_id, response)
