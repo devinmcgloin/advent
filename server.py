ace527b62f383f04c74814f94871ababee70785d
Devin McGloin <devin@devinmcgloin.com>
2016-06-25 13:59:11 -0400

    registering webhooks

--- server.py
+++ server.py
@@ -102,6 +102,6 @@ def setup_webhooks():
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s - %(levelname)s - %(filename)s - %(lineno)d  - %(message)s')
-    # setup_webhooks()
+    setup_webhooks()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
